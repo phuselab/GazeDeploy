9cbf0ba58f9bb27dd3e81394062360914692fb77
Alessandro D'Amelio <32065441+aledamelio@users.noreply.github.com>
2021-03-10 23:12:41 +0100

    Update build_face_maps.sh

--- build_face_maps.sh
+++ build_face_maps.sh
@@ -29,6 +29,7 @@ out_scores="${out_dir}pywork/${ref_name}/"
 tracks_dir="${out_dir}pywork/${ref_name}/tracks.pckl"
 scores_dir="${out_scores}whospeaks.npy"
 out_video="${out_dir}pyavi/${ref_name}/"
+face_maps="speaker_detect/data/face_maps/"
 face_maps_dir="speaker_detect/data/face_maps/${ref_name}/"
 #STS_dir="speaker_detect/data/ST_maps/${ref_name}/"
 
@@ -37,6 +38,7 @@ python speaker_detect/run_pipeline_fixed.py --videofile $videofile --reference $
 python speaker_detect/run_syncnet_fixed.py --videofile $videofile --reference $ref_name --data_dir $out_dir
 python speaker_detect/get_speaker_score.py --video_feats $V_feats_dir --audio_feats $A_feats_dir --out_scores $out_scores
 
+mkdir $face_maps
 mkdir $face_maps_dir
 #mkdir $STS_dir
 python create_face_maps.py --tracks $tracks_dir --video $videofile --scores $scores_dir --out_video $out_video --maps_out $face_maps_dir
